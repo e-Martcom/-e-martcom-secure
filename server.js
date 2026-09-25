ff5312246694763cad7571e19d6da8db761ebd76
e-Martcom <ndegwandirangu22@gmail.com>
2026-09-25 20:10:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log('Live on ' + PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('Live on ' + PORT));
