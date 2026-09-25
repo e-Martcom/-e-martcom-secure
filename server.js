d6689e5938dbd88f0d4a80c9badc087a3d166819
e-Martcom <ndegwandirangu22@gmail.com>
2026-09-25 20:00:18 +0300

    Refactor sendFile to use path.join
    const express = require('express');
    const path = require('path');
    const app = express();
    app.use(express.static('public'));
    app.get('/', (req, res) => {
      res.sendFile(path.join(__dirname, 'public', 'index.html'));
    });
    const PORT = process.env.PORT || 10000;
    app.listen(PORT, () => console.log('Live on ' + PORT));

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-app.get('/api/test', (req, res) => {
-  res.json({ ok: true });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log('Live'));
+app.listen(PORT, () => console.log('Live on ' + PORT));
